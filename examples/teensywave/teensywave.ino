22a70ffd02495934b91547a82d2cbd908e084d73
Tinkerspy <tinkerspy@myown.mailcan.com>
2015-08-23 21:59:07 +0200

    Updated teensywave example

--- examples/teensywave/teensywave.ino
+++ examples/teensywave/teensywave.ino
@@ -2,11 +2,11 @@
 #include <Atm_teensywave.h>
 #include <Atm_button.h>
 
-// A state machine generates simple waveforms on Teensy 3.1
+// A state machine generates simple waveforms on Teensy 3.1/LC (which has true analog out)
 // Attach a button to pin 11 to cycle thru SINE (SN) > SAW (SW) > SQUARE (SQ)
 
-int pinOut = A14;
-int pinIn = 11;
+int pinOut = A14; // Teensy 3.1 analog output pin (LC uses A12)
+int pinIn = 11;   // Button attached to pin 11 (pullup)
 
 Atm_teensywave wave;
 Atm_button btn;
@@ -20,7 +20,8 @@ void btnHandler( int press ) {
 
 void setup()
 {
-  wave.begin( pinOut, 314, 50 ).state( wave.START_SN ); // Start in SINE mode (SN)
+  // 314 steps, 50 usec period, start machine in SN (Sine Wave) state
+  wave.begin( pinOut, 314, 50 ).state( wave.START_SN ); 
   btn.begin( pinIn, btnHandler );
   factory.add( wave ).add( btn );
 }
