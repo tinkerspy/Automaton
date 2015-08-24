f119093d52e77cdc335774b0bb1b6424038a2423
Tinkerspy <tinkerspy@myown.mailcan.com>
2015-08-24 13:22:12 +0200

    Formatting

--- Atm_teensywave.cpp
+++ Atm_teensywave.cpp
@@ -13,28 +13,28 @@ ATM_CLASSNAME & ATM_CLASSNAME::begin( int attached_pin, int steps, int delay )
       /* START_SQ */ ACT_STARTSQ,        -1,      -1,          -1,         -1,         -1,  SQON,
       /* SQON     */    ACT_SQON,        -1,      -1,          -1,      SQOFF,         -1,    -1,
       /* SQOFF    */   ACT_SQOFF,        -1,      -1,          -1,       SQON,   START_SN,    -1,
-	  };
+      };
       table( state_table, ELSE );
       pin = attached_pin; 
       _steps = steps;
-	  _delay = delay;
+      _delay = delay;
       set( timer, delay ); 
       set( phase, steps ); // 314 steps of 0.02 = 6.28 ( 2 * pi )
       _stepsize = (float) 6.28318 / _steps;
       pinMode( pin, OUTPUT ); 
-	  analogWriteResolution( 12 );
+      analogWriteResolution( 12 );
       return *this;          
 }
 
 int ATM_CLASSNAME::event( int id ) 
 {
   switch ( id ) {
-	case EVT_TIMER :
-	  return expired( timer );        
-	case EVT_COUNTER :
-	  return expired( phase );        
-	case EVT_TOGGLE :
-	  return signalRead( SIG_TOGGLE );
+    case EVT_TIMER :
+      return expired( timer );        
+    case EVT_COUNTER :
+      return expired( phase );        
+    case EVT_TOGGLE :
+      return signalRead( SIG_TOGGLE );
    }
    return 0;
 }
@@ -43,31 +43,31 @@ void ATM_CLASSNAME::action( int id )
 {
   int val;
   switch ( id ) {
-	case ACT_IDLE :
-	  analogWrite( pin, 0 );
-	  return;
-	case ACT_START :
+    case ACT_IDLE :
+      analogWrite( pin, 0 );
+      return;
+    case ACT_START :
       set( timer, _delay ); 
-	  set( phase, _steps );
-	  return;
-	case ACT_STARTSQ :
+      set( phase, _steps );
+      return;
+    case ACT_STARTSQ :
       set( timer, (int) ( _steps * _delay / 2 ) ); 
-	  return;
-	case ACT_SINE :        
-	  val = sin( phase.value * _stepsize ) * 2000.0 + 2050.0;
-	  analogWrite( pin, val );
-	  decrement( phase );
-	  return;
-	case ACT_SAW :        
-	  val = ( _steps - phase.value ) * 13;
-	  analogWrite( pin, val );
-	  decrement( phase );
-	  return;
-	case ACT_SQON :        
-	  analogWrite( pin, 4095 );
-	  return;
-	case ACT_SQOFF :        
-	  analogWrite( pin, 0 );
-	  return;
+      return;
+    case ACT_SINE :        
+      val = sin( phase.value * _stepsize ) * 2000.0 + 2050.0;
+      analogWrite( pin, val );
+      decrement( phase );
+      return;
+    case ACT_SAW :        
+      val = ( _steps - phase.value ) * 13;
+      analogWrite( pin, val );
+      decrement( phase );
+      return;
+    case ACT_SQON :        
+      analogWrite( pin, 4095 );
+      return;
+    case ACT_SQOFF :        
+      analogWrite( pin, 0 );
+      return;
    }
 }
