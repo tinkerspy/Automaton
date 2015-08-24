490ffaf6a04ff9ce7564f16e552b7f19091e1570
Tinkerspy <tinkerspy@myown.mailcan.com>
2015-08-24 17:32:01 +0200

    Fixed teensywave example

--- examples/teensywave/teensywave.ino
+++ examples/teensywave/teensywave.ino
@@ -3,7 +3,7 @@
 #include <Atm_button.h>
 
 // A state machine generates simple waveforms on Teensy 3.1/LC (which has true analog out)
-// Attach a button to pin 11 to cycle thru SINE (SN) > SAW (SW) > SQUARE (SQ)
+// Attach a button to pin 11 to cycle thru SINE (SN) > SAW (SW) > SAW REVERSE (SR) > TRIANGLE (TR) > SQUARE (SQ)
 
 int pinOut = A14; // Teensy 3.1 analog output pin (LC uses A12)
 int pinIn = 11;   // Button attached to pin 11 (pullup)
