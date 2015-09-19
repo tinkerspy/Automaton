645ff9b91a567eb710bb0538ba3ad102df5692f1
Tinkerspy <tinkerspy@myown.mailcan.com>
2015-09-19 15:02:08 +0200

    Added atm_timer state machine

---
+++ Atm_timer.cpp
@@ -0,0 +1,76 @@
+#include <Automaton.h>
+#include "Atm_timer.h"
+	
+Atm_timer & Atm_timer::begin( void )
+{
+  const static state_t state_table[] PROGMEM = {
+  /*             ON_ENTER    ON_LOOP  ON_EXIT  EVT_TIMER   EVT_COUNTER  EVT_OFF  EVT_ON  ELSE */
+  /* IDLE    */        -1,        -1,      -1,        -1,           -1,      -1,   WAIT,   -1,
+  /* WAIT    */        -1,        -1,      -1,   TRIGGER,           -1,    IDLE,     -1,   -1,
+  /* TRIGGER */  ACT_TRIG,        -1,      -1,        -1,         IDLE,    IDLE,     -1, WAIT,
+  };
+  Machine::begin( state_table, ELSE );
+  Machine::msgQueue( messages, MSG_END );
+  set( timer, ATM_TIMER_OFF ); 
+  set( timer, ATM_COUNTER_OFF ); 
+  return *this;          
+}
+
+
+Atm_timer & Atm_timer::onTimer( Machine * machine, uint8_t msg ) 
+{
+  client_machine = machine;
+  client_msg = msg;
+  return *this;  
+}
+
+Atm_timer & Atm_timer::onTimer( timer_cb_t timer_callback ) 
+{
+  callback = timer_callback;
+  return *this;  
+}
+
+Atm_timer & Atm_timer::interval( int v )
+{
+  set( timer, v );  
+}
+
+Atm_timer & Atm_timer::repeat( int v )
+{
+  set( counter, v );  
+}
+
+Atm_timer & Atm_timer::id( int v )
+{
+  timer_id = v;  
+}
+
+int Atm_timer::event( int id ) 
+{
+  switch ( id ) {
+    case EVT_COUNTER :
+      return expired( counter );        
+    case EVT_TIMER :
+      return expired( timer );        
+    case EVT_OFF :
+      return msgRead( MSG_OFF );        
+    case EVT_ON :
+      return msgRead( MSG_ON );        
+   }
+   return 0;
+}
+
+void Atm_timer::action( int id ) 
+{
+  switch ( id ) {
+  	case ACT_TRIG :
+      decrement( counter );
+      if ( callback ) {
+         (*callback)( timer_id );
+      }
+      if ( client_machine ) {
+        client_machine->msgWrite( client_msg );
+      }
+      return;
+   }
+}
---
+++ Atm_timer.h
@@ -0,0 +1,38 @@
+
+#ifndef Atm_timer_h
+#define Atm_timer_h
+
+#include <Automaton.h>
+
+typedef void (*timer_cb_t)( int );
+
+class Atm_timer: public Machine {
+
+  public:
+    Atm_timer( void ) : Machine() { class_label = "TMR"; };
+
+    atm_milli_timer timer;
+    atm_counter counter;
+    int timer_id;
+    timer_cb_t callback = 0;
+    Machine * client_machine;
+    uint8_t client_msg;
+
+    enum { IDLE, WAIT, TRIGGER } STATES;
+    enum { EVT_TIMER, EVT_COUNTER, EVT_OFF, EVT_ON, ELSE } EVENTS;
+	  enum { ACT_TRIG } ACTIONS;
+    enum { MSG_OFF, MSG_ON, MSG_END } MESSAGES;
+
+    atm_msg_t messages[MSG_END];
+	
+    Atm_timer & begin( void );
+    Atm_timer & onTimer( Machine * machine, uint8_t msg );
+    Atm_timer & onTimer( timer_cb_t timer_callback ); 
+    Atm_timer & interval( int v );
+    Atm_timer & repeat( int v );
+    Atm_timer & id( int v );
+    int event( int id ); 
+    void action( int id ); 
+};
+
+#endif
