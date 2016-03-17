9feb082ff33fc3393bfae74ee2402faf9393e619
Tinkerspy <tinkerspy@myown.mailcan.com>
2016-03-17 11:53:25 +0100

    Atm_timer: added interval_seconds(), now supports timers up to 137 years

--- Atm_timer.cpp
+++ Atm_timer.cpp
@@ -1,20 +1,32 @@
-#include <Automaton.h>
 #include "Atm_timer.h"
-	
-Atm_timer & Atm_timer::begin( void )
+
+// Timer class that handles intervals from 1 millisecond up to 136 years
+// Combined with repeat( 65534 ) that makes a maximum of 8.9 million years
+
+#define DIVIDER 86400 // Number of seconds in a 24h day
+  
+Atm_timer & Atm_timer::begin( uint32_t ms )
 {
   const static state_t state_table[] PROGMEM = {
-  /*             ON_ENTER    ON_LOOP  ON_EXIT  EVT_TIMER   EVT_COUNTER  EVT_OFF  EVT_ON  ELSE */
-  /* IDLE    */        -1, ATM_SLEEP,      -1,        -1,           -1,      -1,   WAIT,   -1,
-  /* WAIT    */        -1,        -1,      -1,   TRIGGER,           -1,    IDLE,     -1,   -1,
-  /* TRIGGER */  ACT_TRIG,        -1,      -1,        -1,         IDLE,    IDLE,     -1, WAIT,
+  /*             ON_ENTER    ON_LOOP    ON_EXIT  EVT_DAYCNT  EVT_DAYTIMER  EVT_MSTIMER  EVT_REPCNT  EVT_OFF  EVT_ON   ELSE */
+  /* IDLE    */        -1, ATM_SLEEP,        -1,         -1,           -1,          -1,         -1,      -1,  START,    -1,
+  /* START   */ ACT_START,        -1,        -1,         -1,           -1,          -1,         -1,      -1,  WAITD, WAITD,  
+  /* WAITD   */        -1,        -1, ACT_WAITD,     WAITMS,        WAITD,          -1,         -1,    IDLE,  START,    -1,
+  /* WAITMS  */        -1,        -1,        -1,         -1,           -1,     TRIGGER,         -1,    IDLE,  START,    -1,
+  /* TRIGGER */  ACT_TRIG,        -1,        -1,         -1,           -1,          -1,       IDLE,    IDLE,  START, START,
   };
   Machine::begin( state_table, ELSE );
   Machine::msgQueue( messages, MSG_END, 1 );
-  timer.begin( this, ATM_TIMER_OFF ); 
-  counter.set( 1 ); 
-  state( WAIT );
+  daytimer.begin( this, (uint32_t) DIVIDER * 1000 ); // Always set to one day
+  mstimer.begin( this, ms );
+  daycounter.set( days = 0 );
+  repeat( repeat_cnt = 1 );
   return *this;          
+}
+
+Atm_timer & Atm_timer::begin( void )
+{
+  begin( ATM_TIMER_OFF );
 }
 
 
@@ -31,17 +43,34 @@ Atm_timer & Atm_timer::onTimer( timer_cb_t timer_callback )
   return *this;  
 }
 
+Atm_timer & Atm_timer::interval_seconds( uint32_t v )
+{
+  days = v /  ( (uint32_t) DIVIDER );
+  daycounter.set( days); // Determine how many days -> days => Set day counter
+  mstimer.set( ( v - ( (uint32_t) days * DIVIDER ) ) * 1000 );  // And how many milliseconds left 
+  state( START );
+  return *this;
+}
+
+Atm_timer & Atm_timer::interval_millis( uint32_t v )
+{
+  days = 0;
+  daycounter.set( days ); 
+  mstimer.set( v );  
+  state( START );
+  return *this;
+}
+
 Atm_timer & Atm_timer::interval( uint32_t v )
 {
-  timer.set( v );  
-  state( WAIT );
-  return *this;
+  interval_millis( v );
 }
 
 Atm_timer & Atm_timer::repeat( int v )
 {
-  counter.set( v );  
-  state( WAIT );
+  repeat_cnt = v;
+  repcounter.set( v );  
+  state( START );
   return *this;
 }
 
@@ -54,10 +83,14 @@ Atm_timer & Atm_timer::id( int v )
 int Atm_timer::event( int id ) 
 {
   switch ( id ) {
-    case EVT_COUNTER :
-      return counter.expired();        
-    case EVT_TIMER :
-      return timer.expired();        
+    case EVT_REPCNT :
+      return repcounter.expired();        
+    case EVT_DAYCNT :
+      return daycounter.expired();        
+    case EVT_MSTIMER :
+      return mstimer.expired();        
+    case EVT_DAYTIMER :
+      return daytimer.expired();        
     case EVT_OFF :
       return msgRead( MSG_OFF );        
     case EVT_ON :
@@ -69,10 +102,16 @@ int Atm_timer::event( int id )
 void Atm_timer::action( int id ) 
 {
   switch ( id ) {
+    case ACT_START :
+      daycounter.set( days );
+      return;
+    case ACT_WAITD :
+      daycounter.decrement();
+      return;
   	case ACT_TRIG :
-      counter.decrement();
+      repcounter.decrement();
       if ( callback ) {
-         (*callback)( timer_id );
+         (*callback)( timer_id, repeat_cnt - repcounter.value );
       }
       if ( client_machine ) {
         client_machine->msgWrite( client_msg );
@@ -84,28 +123,35 @@ void Atm_timer::action( int id )
 Atm_timer & Atm_timer::onSwitch( swcb_sym_t switch_callback ) {
 
   Machine::onSwitch( switch_callback, 
-    "IDLE\0WAIT\0TRIGGER", 
-    "EVT_TIMER\0EVT_COUNTER\0EVT_OFF\0EVT_ON\0ELSE" );  
+    "IDLE\0START\0WAITD\0WAITMS\0TRIGGER",
+    "EVT_DAYCNT\0EVT_DAYTIMER\0EVT_MSTIMER\0EVT_REPCNT\0EVT_OFF\0EVT_ON\0ELSE" );    
   return *this;
 }
 
 // TinyMachine version
-	
-Att_timer & Att_timer::begin( void )
+  
+Att_timer & Att_timer::begin( uint32_t ms )
 {
   const static tiny_state_t state_table[] PROGMEM = {
-  /*             ON_ENTER    ON_LOOP  ON_EXIT  EVT_TIMER   EVT_COUNTER  EVT_OFF  EVT_ON  ELSE */
-  /* IDLE    */        -1, ATM_SLEEP,      -1,        -1,           -1,      -1,   WAIT,   -1,
-  /* WAIT    */        -1,        -1,      -1,   TRIGGER,           -1,    IDLE,     -1,   -1,
-  /* TRIGGER */  ACT_TRIG,        -1,      -1,        -1,         IDLE,    IDLE,     -1, WAIT,
+  /*             ON_ENTER    ON_LOOP    ON_EXIT  EVT_DAYCNT  EVT_DAYTIMER  EVT_MSTIMER  EVT_REPCNT  EVT_OFF  EVT_ON   ELSE */
+  /* IDLE    */        -1, ATM_SLEEP,        -1,         -1,           -1,          -1,         -1,      -1,  START,    -1,
+  /* START   */ ACT_START,        -1,        -1,         -1,           -1,          -1,         -1,      -1,  WAITD, WAITD,  
+  /* WAITD   */        -1,        -1, ACT_WAITD,     WAITMS,        WAITD,          -1,         -1,    IDLE,  START,    -1,
+  /* WAITMS  */        -1,        -1,        -1,         -1,           -1,     TRIGGER,         -1,    IDLE,  START,    -1,
+  /* TRIGGER */  ACT_TRIG,        -1,        -1,         -1,           -1,          -1,       IDLE,    IDLE,  START, START,
   };
   TinyMachine::begin( state_table, ELSE );
-  timer.begin( this, ATM_TIMER_OFF ); 
-  counter.set( 1 ); 
-  state( WAIT );
+  daytimer.begin( this, (uint32_t) DIVIDER * 1000 ); // Always set to one day
+  mstimer.begin( this, ms );
+  daycounter.set( days = 0 );
+  repeat( repeat_cnt = 1 );
   return *this;          
 }
 
+Att_timer & Att_timer::begin( void )
+{
+  begin( ATM_TIMER_OFF );
+}
 
 Att_timer & Att_timer::onTimer( Machine * machine, uint8_t msg ) 
 {
@@ -120,17 +166,34 @@ Att_timer & Att_timer::onTimer( timer_cb_t timer_callback )
   return *this;  
 }
 
+Att_timer & Att_timer::interval_seconds( uint32_t v )
+{
+  days = v /  ( (uint32_t) DIVIDER );
+  daycounter.set( days); // Determine how many days -> days => Set day counter
+  mstimer.set( ( v - ( (uint32_t) days * DIVIDER ) ) * 1000 );  // And how many milliseconds left 
+  state( START );
+  return *this;
+}
+
+Att_timer & Att_timer::interval_millis( uint32_t v )
+{
+  days = 0;
+  daycounter.set( days ); 
+  mstimer.set( v );  
+  state( START );
+  return *this;
+}
+
 Att_timer & Att_timer::interval( uint32_t v )
 {
-  timer.set( v );  
-  state( WAIT );
-  return *this;
+  interval_millis( v );
 }
 
 Att_timer & Att_timer::repeat( int v )
 {
-  counter.set( v );  
-  state( WAIT );
+  repeat_cnt = v;
+  repcounter.set( v );  
+  state( START );
   return *this;
 }
 
@@ -143,10 +206,14 @@ Att_timer & Att_timer::id( int v )
 int Att_timer::event( int id ) 
 {
   switch ( id ) {
-    case EVT_COUNTER :
-      return counter.expired();        
-    case EVT_TIMER :
-      return timer.expired();        
+    case EVT_REPCNT :
+      return repcounter.expired();        
+    case EVT_DAYCNT :
+      return daycounter.expired();        
+    case EVT_MSTIMER :
+      return mstimer.expired();        
+    case EVT_DAYTIMER :
+      return daytimer.expired();        
    }
    return 0;
 }
@@ -154,10 +221,16 @@ int Att_timer::event( int id )
 void Att_timer::action( int id ) 
 {
   switch ( id ) {
+    case ACT_START :
+      daycounter.set( days );
+      return;
+    case ACT_WAITD :
+      daycounter.decrement();
+      return;
   	case ACT_TRIG :
-      counter.decrement();
+      repcounter.decrement();
       if ( callback ) {
-         (*callback)( timer_id );
+         (*callback)( timer_id, repeat_cnt - repcounter.value );
       }
       if ( client_machine ) {
         client_machine->msgWrite( client_msg );
@@ -165,5 +238,3 @@ void Att_timer::action( int id )
       return;
    }
 }
-
-
--- Atm_timer.h
+++ Atm_timer.h
@@ -4,31 +4,36 @@
 
 #include <Automaton.h>
 
-typedef void (*timer_cb_t)( int );
+typedef void (*timer_cb_t)( int, uint16_t );
 
 class Atm_timer: public Machine {
 
   public:
     Atm_timer( void ) : Machine() { class_label = "TMR"; };
 
-    atm_timer_millis timer;
-    atm_counter counter;
+    atm_timer_millis daytimer, mstimer;
+    atm_counter daycounter, repcounter;
     int timer_id;
     timer_cb_t callback = 0;
     Machine * client_machine;
     uint8_t client_msg;
-
-    enum { IDLE, WAIT, TRIGGER } STATES;
-    enum { EVT_TIMER, EVT_COUNTER, EVT_OFF, EVT_ON, ELSE } EVENTS;
-	  enum { ACT_TRIG } ACTIONS;
+    uint16_t days; 
+    uint16_t repeat_cnt; 
+    
+    enum { IDLE, START, WAITD, WAITMS, TRIGGER } STATES;
+    enum { EVT_DAYCNT, EVT_DAYTIMER, EVT_MSTIMER, EVT_REPCNT, EVT_OFF, EVT_ON, ELSE } EVENTS;
+	  enum { ACT_START, ACT_TRIG, ACT_WAITD } ACTIONS;
     enum { MSG_OFF, MSG_ON, MSG_END } MESSAGES;
 
     atm_msg_t messages[MSG_END];
 	
     Atm_timer & begin( void );
+    Atm_timer & begin( uint32_t ms );
     Atm_timer & onSwitch( swcb_sym_t switch_callback );
     Atm_timer & onTimer( Machine * machine, uint8_t msg );
     Atm_timer & onTimer( timer_cb_t timer_callback ); 
+    Atm_timer & interval_seconds( uint32_t v );
+    Atm_timer & interval_millis( uint32_t v );
     Atm_timer & interval( uint32_t v );
     Atm_timer & repeat( int v );
     Atm_timer & id( int v );
@@ -36,33 +41,39 @@ class Atm_timer: public Machine {
     void action( int id ); 
 };
 
-// TinyMachine version
+// Tiny Machine version
 
 class Att_timer: public TinyMachine {
 
   public:
     Att_timer( void ) : TinyMachine() { };
 
-    atm_timer_millis timer;
-    atm_counter counter;
+    atm_timer_millis daytimer, mstimer;
+    atm_counter daycounter, repcounter;
     int timer_id;
     timer_cb_t callback = 0;
     Machine * client_machine;
     uint8_t client_msg;
-
-    enum { IDLE, WAIT, TRIGGER } STATES;
-    enum { EVT_TIMER, EVT_COUNTER, EVT_OFF, EVT_ON, ELSE } EVENTS;
-	enum { ACT_TRIG } ACTIONS;
+    uint16_t days; 
+    uint16_t repeat_cnt; 
+    
+    enum { IDLE, START, WAITD, WAITMS, TRIGGER } STATES;
+    enum { EVT_DAYCNT, EVT_DAYTIMER, EVT_MSTIMER, EVT_REPCNT, EVT_OFF, EVT_ON, ELSE } EVENTS;
+    enum { ACT_START, ACT_TRIG, ACT_WAITD } ACTIONS;
 	
     Att_timer & begin( void );
+    Att_timer & begin( uint32_t ms );
     Att_timer & onTimer( Machine * machine, uint8_t msg );
     Att_timer & onTimer( timer_cb_t timer_callback ); 
+    Att_timer & interval_seconds( uint32_t v );
+    Att_timer & interval_millis( uint32_t v );
     Att_timer & interval( uint32_t v );
     Att_timer & repeat( int v );
     Att_timer & id( int v );
     int event( int id ); 
     void action( int id ); 
 };
+
 
 
 #endif
