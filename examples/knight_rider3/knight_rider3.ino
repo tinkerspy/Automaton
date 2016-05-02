469b38dc1a8ed25dd5a5cb883bbb9d15c8a3be7e
Tinkerspy <tinkerspy@myown.mailcan.com>
2016-05-02 08:45:49 +0200

    Comma!

--- examples/knight_rider3/knight_rider3.ino
+++ examples/knight_rider3/knight_rider3.ino
@@ -9,7 +9,7 @@ Factory factory;
 void setup() {
   // Create a sweep machine and add it to the factory
   factory.add( sweep.begin( 4, 5, 6, 7, 8, 9 ).speed( 50 ) );
-  // Create a button machine link it to the sweep machine and add it to the factory
+  // Create a button machine, link it to the sweep machine and add it to the factory
   factory.add( button.begin( 2 ).onPress( sweep, sweep.EVT_TOGGLE ) );
 }
 
