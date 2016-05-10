113d50efaef921aff1757a0f6f52aad0c2156de2
Tinkerspy <tinkerspy@myown.mailcan.com>
2016-05-10 15:38:40 +0200

    Atm_pulse: converted to atm_connector

--- Atm_pulse.cpp
+++ Atm_pulse.cpp
@@ -17,61 +17,43 @@ Atm_pulse& Atm_pulse::begin( int attached_pin, int minimum_duration /* = 20 */, 
   return *this;
 }
 
-Atm_pulse& Atm_pulse::onPulse( pulsecb_t callback, int idx /* = 0 */ ) {
-  _onpulse._mode = MODE_CALLBACK;
-  _onpulse._callback = callback;
-  _onpulse._callback_idx = idx;
-  _onpulse._callback_count = 0;
+Atm_pulse& Atm_pulse::onPulse( atm_cb_t callback, int idx /* = 0 */ ) {
+  _onpulse.set( callback, idx );
   return *this;
 }
 
 Atm_pulse& Atm_pulse::onPulse( Machine& machine, int event /* = 0 */ ) {
-  _onpulse._mode = MODE_MACHINE;
-  _onpulse._client_machine = &machine;
-  _onpulse._client_machine_event = event;
+  _onpulse.set( &machine, event );
   return *this;
 }
 
 Atm_pulse& Atm_pulse::onPulse( const char* label, int event /* = 0 */ ) {
-  _onpulse._mode = MODE_FACTORY;
-  _onpulse._client_label = label;
-  _onpulse._client_label_event = event;
+  _onpulse.set( label, event );
   return *this;
 }
 
 Atm_pulse& Atm_pulse::onPulse( TinyMachine& machine, int event /* = 0 */ ) {
-  _onpulse._mode = MODE_TMACHINE;
-  _onpulse._client_tmachine = &machine;
-  _onpulse._client_tmachine_event = event;
+  _onpulse.set( &machine, event );
   return *this;
 }
 
-Atm_pulse& Atm_pulse::onRelease( pulsecb_t callback, int idx /* = 0 */ ) {
-  _onrelease._mode = MODE_CALLBACK;
-  _onrelease._callback = callback;
-  _onrelease._callback_idx = idx;
-  _onrelease._callback_count = 0;
+Atm_pulse& Atm_pulse::onRelease( atm_cb_t callback, int idx /* = 0 */ ) {
+  _onrelease.set( callback, idx );
   return *this;
 }
 
 Atm_pulse& Atm_pulse::onRelease( Machine& machine, int event /* = 0 */ ) {
-  _onrelease._mode = MODE_MACHINE;
-  _onrelease._client_machine = &machine;
-  _onrelease._client_machine_event = event;
+  _onrelease.set( &machine, event );
   return *this;
 }
 
 Atm_pulse& Atm_pulse::onRelease( const char* label, int event /* = 0 */ ) {
-  _onrelease._mode = MODE_FACTORY;
-  _onrelease._client_label = label;
-  _onrelease._client_label_event = event;
+  _onrelease.set( label, event );
   return *this;
 }
 
 Atm_pulse& Atm_pulse::onRelease( TinyMachine& machine, int event /* = 0 */ ) {
-  _onrelease._mode = MODE_TMACHINE;
-  _onrelease._client_tmachine = &machine;
-  _onrelease._client_tmachine_event = event;
+  _onrelease.set( &machine, event );
   return *this;
 }
 
@@ -90,34 +72,10 @@ int Atm_pulse::event( int id ) {
 void Atm_pulse::action( int id ) {
   switch ( id ) {
     case ACT_PULSE:
-      if ( _onpulse._mode == MODE_CALLBACK ) {
-        _onpulse._callback_count++;
-        ( *_onpulse._callback )( _onpulse._callback_idx, _onpulse._callback_count );
-      }
-      if ( _onpulse._mode == MODE_MACHINE ) {
-        _onpulse._client_machine->trigger( _onpulse._client_machine_event );
-      }
-      if ( _onpulse._mode == MODE_FACTORY && factory ) {
-        factory->trigger( _onpulse._client_label, _onpulse._client_label_event );
-      }
-      if ( _onpulse._mode == MODE_TMACHINE ) {
-        _onpulse._client_tmachine->trigger( _onpulse._client_tmachine_event );
-      }
+      _onpulse.push();
       return;
     case ACT_RELEASE:
-      if ( _onrelease._mode == MODE_CALLBACK ) {
-        _onrelease._callback_count++;
-        ( *_onrelease._callback )( _onrelease._callback_idx, _onrelease._callback_count );
-      }
-      if ( _onrelease._mode == MODE_MACHINE ) {
-        _onrelease._client_machine->trigger( _onrelease._client_machine_event );
-      }
-      if ( _onrelease._mode == MODE_FACTORY && factory ) {
-        factory->trigger( _onrelease._client_label, _onrelease._client_label_event );
-      }
-      if ( _onrelease._mode == MODE_TMACHINE ) {
-        _onrelease._client_tmachine->trigger( _onrelease._client_tmachine_event );
-      }
+      _onrelease.push();
       return;
   }
 }
@@ -129,101 +87,3 @@ Atm_pulse& Atm_pulse::trace( Stream& stream ) {
 
 // TinyMachine version
 
-Att_pulse& Att_pulse::begin( int attached_pin, int minimum_duration /* = 20 */, bool activeLow /* = false */, bool pullUp /* = false */ ) {
-  const static tiny_state_t state_table[] PROGMEM = {/*              ON_ENTER    ON_LOOP      ON_EXIT  EVT_TIMER   EVT_HIGH  EVT_LOW   ELSE */
-                                                     /* IDLE    */ -1, -1, -1, -1, WAIT, -1, -1,
-                                                     /* WAIT    */ -1, -1, -1, PULSE, -1, IDLE, -1,
-                                                     /* PULSE   */ ACT_PULSE, -1, ACT_RELEASE, -1, -1, IDLE, -1,
-  };
-  TinyMachine::begin( state_table, ELSE );
-  pin = attached_pin;
-  _activeLow = activeLow;
-  timer.set( minimum_duration );
-  pinMode( pin, pullUp ? INPUT_PULLUP : INPUT );
-  return *this;
-}
-
-Att_pulse& Att_pulse::onPulse( pulsecb_t callback, int idx /* = 0 */ ) {
-  _onpulse._mode = MODE_CALLBACK;
-  _onpulse._callback = callback;
-  _onpulse._callback_idx = idx;
-  _onpulse._callback_count = 0;
-  return *this;
-}
-
-Att_pulse& Att_pulse::onPulse( Machine& machine, int event /* = 0 */ ) {
-  _onpulse._mode = MODE_MACHINE;
-  _onpulse._client_machine = &machine;
-  _onpulse._client_machine_event = event;
-  return *this;
-}
-
-Att_pulse& Att_pulse::onPulse( TinyMachine& machine, int event /* = 0 */ ) {
-  _onpulse._mode = MODE_TMACHINE;
-  _onpulse._client_tmachine = &machine;
-  _onpulse._client_tmachine_event = event;
-  return *this;
-}
-
-Att_pulse& Att_pulse::onRelease( pulsecb_t callback, int idx /* = 0 */ ) {
-  _onrelease._mode = MODE_CALLBACK;
-  _onrelease._callback = callback;
-  _onrelease._callback_idx = idx;
-  _onrelease._callback_count = 0;
-  return *this;
-}
-
-Att_pulse& Att_pulse::onRelease( Machine& machine, int event /* = 0 */ ) {
-  _onrelease._mode = MODE_MACHINE;
-  _onrelease._client_machine = &machine;
-  _onrelease._client_machine_event = event;
-  return *this;
-}
-
-Att_pulse& Att_pulse::onRelease( TinyMachine& machine, int event /* = 0 */ ) {
-  _onrelease._mode = MODE_TMACHINE;
-  _onrelease._client_tmachine = &machine;
-  _onrelease._client_tmachine_event = event;
-  return *this;
-}
-
-int Att_pulse::event( int id ) {
-  switch ( id ) {
-    case EVT_TIMER:
-      return timer.expired( this );
-    case EVT_HIGH:
-      return ( !digitalRead( pin ) != !_activeLow );  // XOR
-    case EVT_LOW:
-      return !( !digitalRead( pin ) != !_activeLow );
-  }
-  return 0;
-}
-
-void Att_pulse::action( int id ) {
-  switch ( id ) {
-    case ACT_PULSE:
-      if ( _onpulse._mode == MODE_CALLBACK ) {
-        _onpulse._callback_count++;
-        ( *_onpulse._callback )( _onpulse._callback_idx, _onpulse._callback_count );
-      }
-      if ( _onpulse._mode == MODE_MACHINE ) {
-        _onpulse._client_machine->trigger( _onpulse._client_machine_event );
-      }
-      if ( _onpulse._mode == MODE_TMACHINE ) {
-        _onpulse._client_tmachine->trigger( _onpulse._client_tmachine_event );
-      }
-      return;
-    case ACT_RELEASE:
-      if ( _onrelease._mode == MODE_CALLBACK ) {
-        _onrelease._callback_count++;
-        ( *_onrelease._callback )( _onrelease._callback_idx, _onrelease._callback_count );
-      }
-      if ( _onrelease._mode == MODE_MACHINE ) {
-        _onrelease._client_machine->trigger( _onrelease._client_machine_event );
-      }
-      if ( _onrelease._mode == MODE_TMACHINE ) {
-        _onrelease._client_tmachine->trigger( _onrelease._client_tmachine_event );
-      }
-      return;
-  }
-}
--- Atm_pulse.hpp
+++ Atm_pulse.hpp
@@ -7,8 +7,6 @@
 // Detects a pulse (LOW -> HIGH ) on a digital pin with a minimum duration in ms
 // On detection another machine is messaged or a callback is fired
 
-typedef void ( *pulsecb_t )( int idx, uint16_t cnt );
-
 class Atm_Pulse_Comm {
  public:
   uint8_t _mode;
@@ -43,7 +41,7 @@ class Atm_pulse : public Machine {
   int state_high, state_low;
   atm_timer_millis timer;
   bool _activeLow;
-  Atm_Pulse_Comm _onpulse, _onrelease;
+  atm_connector _onpulse, _onrelease;
 
   enum { IDLE, WAIT, PULSE };
   enum { EVT_TIMER, EVT_HIGH, EVT_LOW, ELSE };
@@ -54,43 +52,15 @@ class Atm_pulse : public Machine {
   Atm_pulse& trace( Stream& stream );
   int event( int id );
   void action( int id );
-  Atm_pulse& onPulse( pulsecb_t callback, int idx = 0 );
+  Atm_pulse& onPulse( atm_cb_t callback, int idx = 0 );
   Atm_pulse& onPulse( Machine& machine, int event = 0 );
   Atm_pulse& onPulse( TinyMachine& machine, int event = 0 );
   Atm_pulse& onPulse( const char* label, int event = 0 );
-  Atm_pulse& onRelease( pulsecb_t callback, int idx = 0 );
+  Atm_pulse& onRelease( atm_cb_t callback, int idx = 0 );
   Atm_pulse& onRelease( Machine& machine, int event = 0 );
   Atm_pulse& onRelease( TinyMachine& machine, int event = 0 );
   Atm_pulse& onRelease( const char* label, int event = 0 );
 };
 
 // TinyMachine version
-
-class Att_pulse : public TinyMachine {
- public:
-  Att_pulse( void ) : TinyMachine(){};
-
-  short pin;
-  int state_high, state_low;
-  atm_timer_millis timer;
-  bool _activeLow;
-  Atm_Pulse_Comm _onpulse, _onrelease;
-
-  enum { IDLE, WAIT, PULSE };
-  enum { EVT_TIMER, EVT_HIGH, EVT_LOW, ELSE };
-  enum { ACT_PULSE, ACT_RELEASE };
-  enum { MODE_NULL, MODE_CALLBACK, MODE_MACHINE, MODE_TMACHINE, MODE_FACTORY };
-
-  Att_pulse& begin( int attached_pin, int minimum_duration = 20, bool activeLow = false, bool pullUp = false );
-  Att_pulse& trace( Stream& stream );
-  int event( int id );
-  void action( int id );
-  Att_pulse& onPulse( pulsecb_t callback, int idx = 0 );
-  Att_pulse& onPulse( Machine& machine, int event = 0 );
-  Att_pulse& onPulse( TinyMachine& machine, int event = 0 );
-  Att_pulse& onRelease( pulsecb_t callback, int idx = 0 );
-  Att_pulse& onRelease( Machine& machine, int event = 0 );
-  Att_pulse& onRelease( TinyMachine& machine, int event = 0 );
-};
-
 #endif
