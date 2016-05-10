dcc08b81c33ed1e04faa78d2499b1abb56891dc0
Tinkerspy <tinkerspy@myown.mailcan.com>
2016-05-10 14:37:06 +0200

    Added spaces

--- Atm_led.cpp
+++ Atm_led.cpp
@@ -1,6 +1,6 @@
 #include "Atm_led.hpp"
 
-Atm_led& Atm_led::begin(int attached_pin, bool activeLow) {
+Atm_led& Atm_led::begin( int attached_pin, bool activeLow ) {
   // clang-format off
   static const state_t state_table[] PROGMEM = {
     /*               ON_ENTER    ON_LOOP    ON_EXIT  EVT_ON_TIMER  EVT_OFF_TIMER  EVT_COUNTER  EVT_ON  EVT_OFF  EVT_BLINK  EVT_TOGGLE  EVT_TOGGLE_BLINK ELSE */
@@ -12,25 +12,25 @@ Atm_led& Atm_led::begin(int attached_pin, bool activeLow) {
     /* OFF       */   ACT_OFF,        -1,        -1,           -1,            -1,          -1,     -1,      -1,     START,       IDLE,             IDLE,IDLE, // All off -> IDLE
   };
   // clang-format on
-  Machine::begin(state_table, ELSE);
+  Machine::begin( state_table, ELSE );
   pin = attached_pin;
   _activeLow = activeLow;
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, _activeLow ? HIGH : LOW);
-  on_timer.set(500);
-  off_timer.set(500);
+  pinMode( pin, OUTPUT );
+  digitalWrite( pin, _activeLow ? HIGH : LOW );
+  on_timer.set( 500 );
+  off_timer.set( 500 );
   repeat_count = ATM_COUNTER_OFF;
-  counter.set(repeat_count);
-  while (state() != 0) cycle();
+  counter.set( repeat_count );
+  while ( state() != 0 ) cycle();
   return *this;
 }
 
-Atm_led& Atm_led::onFinish(Machine& n, uint8_t event /* = EVT_BLINK */) {
-  onFinish(n, n, event);
+Atm_led& Atm_led::onFinish( Machine& n, uint8_t event /* = EVT_BLINK */ ) {
+  onFinish( n, n, event );
   return *this;
 }
 
-Atm_led& Atm_led::onFinish(Machine& n, Machine& p, uint8_t event /* = EVT_BLINK */) {
+Atm_led& Atm_led::onFinish( Machine& n, Machine& p, uint8_t event /* = EVT_BLINK */ ) {
   machine_next = &n;
   machine_previous = &p;
   machine_event = event;
@@ -39,12 +39,12 @@ Atm_led& Atm_led::onFinish(Machine& n, Machine& p, uint8_t event /* = EVT_BLINK 
   return *this;
 }
 
-Atm_led& Atm_led::onFinish(TinyMachine& n, uint8_t event /* = EVT_BLINK */) {
-  onFinish(n, n, event);
+Atm_led& Atm_led::onFinish( TinyMachine& n, uint8_t event /* = EVT_BLINK */ ) {
+  onFinish( n, n, event );
   return *this;
 }
 
-Atm_led& Atm_led::onFinish(TinyMachine& n, TinyMachine& p, uint8_t event /* = EVT_BLINK */) {
+Atm_led& Atm_led::onFinish( TinyMachine& n, TinyMachine& p, uint8_t event /* = EVT_BLINK */ ) {
   tmachine_next = &n;
   tmachine_previous = &p;
   machine_event = event;
@@ -53,76 +53,76 @@ Atm_led& Atm_led::onFinish(TinyMachine& n, TinyMachine& p, uint8_t event /* = EV
   return *this;
 }
 
-Atm_led& Atm_led::blink(uint32_t duration, uint32_t pause_duration, uint16_t repeat_count /* = ATM_COUNTER_OFF */) {
-  blink(duration);  // Time in which led is fully on
-  pause(pause_duration);
-  repeat(repeat_count);
+Atm_led& Atm_led::blink( uint32_t duration, uint32_t pause_duration, uint16_t repeat_count /* = ATM_COUNTER_OFF */ ) {
+  blink( duration );  // Time in which led is fully on
+  pause( pause_duration );
+  repeat( repeat_count );
   return *this;
 }
 
-Atm_led& Atm_led::blink(uint32_t duration) {
-  on_timer.set(duration);  // Time in which led is fully on
+Atm_led& Atm_led::blink( uint32_t duration ) {
+  on_timer.set( duration );  // Time in which led is fully on
   return *this;
 }
 
-Atm_led& Atm_led::pause(uint32_t duration) {
-  off_timer.set(duration);  // Time in which led is fully off
+Atm_led& Atm_led::pause( uint32_t duration ) {
+  off_timer.set( duration );  // Time in which led is fully off
   return *this;
 }
 
-Atm_led& Atm_led::fade(int fade) { return *this; }  // Dummy for method compatibility with Atm_fade
+Atm_led& Atm_led::fade( int fade ) { return *this; }  // Dummy for method compatibility with Atm_fade
 
-Atm_led& Atm_led::repeat(int repeat) {
+Atm_led& Atm_led::repeat( int repeat ) {
   repeat_count = repeat >= 0 ? repeat : ATM_COUNTER_OFF;
-  counter.set(repeat_count);
+  counter.set( repeat_count );
   return *this;
 }
 
-int Atm_led::event(int id) {
-  switch (id) {
+int Atm_led::event( int id ) {
+  switch ( id ) {
     case EVT_ON_TIMER:
-      return on_timer.expired(this);
+      return on_timer.expired( this );
     case EVT_OFF_TIMER:
-      return off_timer.expired(this);
+      return off_timer.expired( this );
     case EVT_COUNTER:
       return counter.expired();
   }
   return 0;
 }
 
-void Atm_led::chain_next(void) {
-  if ((flags & ATM_USR2_FLAG) > 0) {
-    machine_next->trigger(machine_event);
+void Atm_led::chain_next( void ) {
+  if ( ( flags & ATM_USR2_FLAG ) > 0 ) {
+    machine_next->trigger( machine_event );
   }
-  if ((flags & ATM_USR4_FLAG) > 0) {
-    tmachine_next->trigger(machine_event);
-  }
-}
-
-void Atm_led::chain_previous(void) {
-  if ((flags & ATM_USR2_FLAG) > 0) {
-    machine_previous->trigger(machine_event);
-  }
-  if ((flags & ATM_USR4_FLAG) > 0) {
-    tmachine_previous->trigger(machine_event);
+  if ( ( flags & ATM_USR4_FLAG ) > 0 ) {
+    tmachine_next->trigger( machine_event );
   }
 }
 
-void Atm_led::action(int id) {
-  switch (id) {
+void Atm_led::chain_previous( void ) {
+  if ( ( flags & ATM_USR2_FLAG ) > 0 ) {
+    machine_previous->trigger( machine_event );
+  }
+  if ( ( flags & ATM_USR4_FLAG ) > 0 ) {
+    tmachine_previous->trigger( machine_event );
+  }
+}
+
+void Atm_led::action( int id ) {
+  switch ( id ) {
     case ACT_INIT:
-      counter.set(repeat_count);
+      counter.set( repeat_count );
       return;
     case ACT_ON:
-      digitalWrite(pin, _activeLow ? LOW : HIGH);
+      digitalWrite( pin, _activeLow ? LOW : HIGH );
       return;
     case ACT_OFF:
       counter.decrement();
-      digitalWrite(pin, _activeLow ? HIGH : LOW);
+      digitalWrite( pin, _activeLow ? HIGH : LOW );
       return;
     case ACT_CHAIN:
-      if ((flags & (ATM_USR2_FLAG | ATM_USR4_FLAG)) > 0) {
-        if ((flags & ATM_USR1_FLAG) > 0) {
+      if ( ( flags & ( ATM_USR2_FLAG | ATM_USR4_FLAG ) ) > 0 ) {
+        if ( ( flags & ATM_USR1_FLAG ) > 0 ) {
           chain_previous();
         } else {
           chain_next();
@@ -133,17 +133,17 @@ void Atm_led::action(int id) {
   }
 }
 
-Atm_led& Atm_led::trace(Stream& stream) {
-  setTrace(&stream, atm_serial_debug::trace,
-           "EVT_ON_TIMER\0EVT_OFF_TIMER\0EVT_COUNTER\0EVT_ON\0EVT_OFF\0EVT_"
-           "BLINK\0EVT_TOGGLE\0EVT_TOGGLE_BLINK\0ELSE\0"
-           "IDLE\0ON\0START\0BLINK_OFF\0DONE\0OFF");
+Atm_led& Atm_led::trace( Stream& stream ) {
+  setTrace( &stream, atm_serial_debug::trace,
+            "EVT_ON_TIMER\0EVT_OFF_TIMER\0EVT_COUNTER\0EVT_ON\0EVT_OFF\0EVT_"
+            "BLINK\0EVT_TOGGLE\0EVT_TOGGLE_BLINK\0ELSE\0"
+            "IDLE\0ON\0START\0BLINK_OFF\0DONE\0OFF" );
   return *this;
 }
 
 // TinyMachine version
 
-Att_led& Att_led::begin(int attached_pin, bool activeLow) {
+Att_led& Att_led::begin( int attached_pin, bool activeLow ) {
   // clang-format off
   static const tiny_state_t state_table[] PROGMEM = {
     /*               ON_ENTER    ON_LOOP    ON_EXIT  EVT_ON_TIMER  EVT_OFF_TIMER  EVT_COUNTER  EVT_ON  EVT_OFF  EVT_BLINK  EVT_TOGGLE  EVT_TOGGLE_BLINK ELSE */
@@ -155,25 +155,25 @@ Att_led& Att_led::begin(int attached_pin, bool activeLow) {
     /* OFF       */   ACT_OFF,        -1,        -1,           -1,            -1,          -1,     -1,      -1,     START,       IDLE,             IDLE,IDLE, // All off -> IDLE
   };
   // clang-format on
-  TinyMachine::begin(state_table, ELSE);
+  TinyMachine::begin( state_table, ELSE );
   pin = attached_pin;
   _activeLow = activeLow;
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, _activeLow ? HIGH : LOW);
-  on_timer.set(500);
-  off_timer.set(500);
+  pinMode( pin, OUTPUT );
+  digitalWrite( pin, _activeLow ? HIGH : LOW );
+  on_timer.set( 500 );
+  off_timer.set( 500 );
   repeat_count = ATM_COUNTER_OFF;
-  counter.set(repeat_count);
-  while (state() != 0) cycle();
+  counter.set( repeat_count );
+  while ( state() != 0 ) cycle();
   return *this;
 }
 
-Att_led& Att_led::onFinish(Machine& n, uint8_t event /* = EVT_BLINK */) {
-  onFinish(n, n, event);
+Att_led& Att_led::onFinish( Machine& n, uint8_t event /* = EVT_BLINK */ ) {
+  onFinish( n, n, event );
   return *this;
 }
 
-Att_led& Att_led::onFinish(Machine& n, Machine& p, uint8_t event /* = EVT_BLINK */) {
+Att_led& Att_led::onFinish( Machine& n, Machine& p, uint8_t event /* = EVT_BLINK */ ) {
   machine_next = &n;
   machine_previous = &p;
   machine_event = event;
@@ -182,12 +182,12 @@ Att_led& Att_led::onFinish(Machine& n, Machine& p, uint8_t event /* = EVT_BLINK 
   return *this;
 }
 
-Att_led& Att_led::onFinish(TinyMachine& n, uint8_t event /* = EVT_BLINK */) {
-  onFinish(n, n, event);
+Att_led& Att_led::onFinish( TinyMachine& n, uint8_t event /* = EVT_BLINK */ ) {
+  onFinish( n, n, event );
   return *this;
 }
 
-Att_led& Att_led::onFinish(TinyMachine& n, TinyMachine& p, uint8_t event /* = EVT_BLINK */) {
+Att_led& Att_led::onFinish( TinyMachine& n, TinyMachine& p, uint8_t event /* = EVT_BLINK */ ) {
   tmachine_next = &n;
   tmachine_previous = &p;
   machine_event = event;
@@ -196,76 +196,76 @@ Att_led& Att_led::onFinish(TinyMachine& n, TinyMachine& p, uint8_t event /* = EV
   return *this;
 }
 
-Att_led& Att_led::blink(uint32_t duration, uint32_t pause_duration, uint16_t repeat_count /* = ATM_COUNTER_OFF */) {
-  blink(duration);  // Time in which led is fully on
-  pause(pause_duration);
-  repeat(repeat_count);
+Att_led& Att_led::blink( uint32_t duration, uint32_t pause_duration, uint16_t repeat_count /* = ATM_COUNTER_OFF */ ) {
+  blink( duration );  // Time in which led is fully on
+  pause( pause_duration );
+  repeat( repeat_count );
   return *this;
 }
 
-Att_led& Att_led::blink(uint32_t duration) {
-  on_timer.set(duration);  // Time in which led is fully on
+Att_led& Att_led::blink( uint32_t duration ) {
+  on_timer.set( duration );  // Time in which led is fully on
   return *this;
 }
 
-Att_led& Att_led::pause(uint32_t duration) {
-  off_timer.set(duration);  // Time in which led is fully off
+Att_led& Att_led::pause( uint32_t duration ) {
+  off_timer.set( duration );  // Time in which led is fully off
   return *this;
 }
 
-Att_led& Att_led::fade(int fade) { return *this; }  // Dummy for method compatibility with Att_fade
+Att_led& Att_led::fade( int fade ) { return *this; }  // Dummy for method compatibility with Att_fade
 
-Att_led& Att_led::repeat(int repeat) {
+Att_led& Att_led::repeat( int repeat ) {
   repeat_count = repeat >= 0 ? repeat : ATM_COUNTER_OFF;
-  counter.set(repeat_count);
+  counter.set( repeat_count );
   return *this;
 }
 
-void Att_led::chain_next(void) {
-  if ((flags & ATM_USR2_FLAG) > 0) {
-    machine_next->trigger(machine_event);
+void Att_led::chain_next( void ) {
+  if ( ( flags & ATM_USR2_FLAG ) > 0 ) {
+    machine_next->trigger( machine_event );
   }
-  if ((flags & ATM_USR4_FLAG) > 0) {
-    tmachine_next->trigger(machine_event);
-  }
-}
-
-void Att_led::chain_previous(void) {
-  if ((flags & ATM_USR2_FLAG) > 0) {
-    machine_previous->trigger(machine_event);
-  }
-  if ((flags & ATM_USR4_FLAG) > 0) {
-    tmachine_previous->trigger(machine_event);
+  if ( ( flags & ATM_USR4_FLAG ) > 0 ) {
+    tmachine_next->trigger( machine_event );
   }
 }
 
-int Att_led::event(int id) {
-  switch (id) {
+void Att_led::chain_previous( void ) {
+  if ( ( flags & ATM_USR2_FLAG ) > 0 ) {
+    machine_previous->trigger( machine_event );
+  }
+  if ( ( flags & ATM_USR4_FLAG ) > 0 ) {
+    tmachine_previous->trigger( machine_event );
+  }
+}
+
+int Att_led::event( int id ) {
+  switch ( id ) {
     case EVT_ON_TIMER:
-      return on_timer.expired(this);
+      return on_timer.expired( this );
     case EVT_OFF_TIMER:
-      return off_timer.expired(this);
+      return off_timer.expired( this );
     case EVT_COUNTER:
       return counter.expired();
   }
   return 0;
 }
 
-void Att_led::action(int id) {
-  switch (id) {
+void Att_led::action( int id ) {
+  switch ( id ) {
     case ACT_INIT:
-      counter.set(repeat_count);
+      counter.set( repeat_count );
       return;
     case ACT_ON:
-      digitalWrite(pin, _activeLow ? LOW : HIGH);
+      digitalWrite( pin, _activeLow ? LOW : HIGH );
       return;
     case ACT_OFF:
       counter.decrement();
-      digitalWrite(pin, _activeLow ? HIGH : LOW);
+      digitalWrite( pin, _activeLow ? HIGH : LOW );
       return;
     case ACT_CHAIN:
-      if ((flags & (ATM_USR2_FLAG | ATM_USR4_FLAG)) > 0) {
-        if ((flags & ATM_USR1_FLAG) > 0) {
+      if ( ( flags & ( ATM_USR2_FLAG | ATM_USR4_FLAG ) ) > 0 ) {
+        if ( ( flags & ATM_USR1_FLAG ) > 0 ) {
           chain_previous();
         } else {
           chain_next();
