8616b58f0e416e6988697258f46c38a321764f61
Tinkerspy <tinkerspy@myown.mailcan.com>
2016-06-03 12:48:16 +0200

    Fixed to state table to handle triggers inside onFinish callback

--- src/Atm_led.cpp
+++ src/Atm_led.cpp
@@ -9,7 +9,7 @@ Atm_led& Atm_led::begin( int attached_pin, bool activeLow ) {
     /* START     */    ENT_ON,        -1,        -1,    BLINK_OFF,            -1,        DONE,     ON,     OFF,     START,        OFF,              OFF,   -1, // Start blinking
     /* BLINK_OFF */   ENT_OFF,        -1,        -1,           -1,         START,        DONE,     ON,     OFF,     START,        OFF,             OFF,   -1,
     /* DONE      */        -1,        -1, EXT_CHAIN,           -1,           OFF,          -1,     ON,     OFF,     START,        OFF,             OFF,   -1, // Wait after last blink
-    /* OFF       */   ENT_OFF,        -1,        -1,           -1,            -1,          -1,     -1,      -1,        -1,         -1,               -1, IDLE, // All off -> IDLE
+    /* OFF       */   ENT_OFF,        -1,        -1,           -1,            -1,          -1,     ON,     OFF,     START,         -1,               -1, IDLE, // All off -> IDLE
   };
   // clang-format on
   Machine::begin( state_table, ELSE );
