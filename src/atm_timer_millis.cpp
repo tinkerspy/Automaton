ef5dea922591360b36308da96a0fa3d37ca71c7f
Steve Jernigan <steve.jernigan@gmail.com>
2024-02-08 22:50:38 -0500

    added elapsed gettor to millis timer

--- src/atm_timer_millis.cpp
+++ src/atm_timer_millis.cpp
@@ -19,6 +19,15 @@ void atm_timer_millis::setFromNow( Machine* machine, uint32_t v ) {
 }
 
 /*
+ * atm_timer_millis::elapsed( this ) - returns the elapsed time so far
+ * This is a rollover-safe 32 bit unsigned integer
+ *
+ */
+uint32_t elapsed( Machine* machine ) {
+    return value == ATM_TIMER_OFF ? 0 : millis() - machine->state_millis
+}
+
+/*
  * atm_timer_millis::expired( this ) - Checks a millis timer for expiry (== 0)
  * This is a rollover-safe 32 bit unsigned integer comparison
  *
